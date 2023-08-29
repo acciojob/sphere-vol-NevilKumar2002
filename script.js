758c90852022c132a1bca2808144b79fdf8da399
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-29 16:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const radius=document.getElementById("radius");
+const volume=document.getElementById("volume");
+
 function volume_sphere() {
     //Write your code here
+	let r=radius.value;
+	let ans=(4/3)*Math.PI*r*r*r;
+	volume.value=ans;
+    // ?volume.value.appendChild(ans);
   
 } 
+volume_sphere();
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
